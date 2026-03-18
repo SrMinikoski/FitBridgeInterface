009528ac2c120e26ede47bc0909f1dadddf9bffa
João Minikoski <jlucasminikoski111@gmail.com>
2026-03-18 20:09:24 -0300

    Limpeza: removendo arquivos duplicados da raiz

--- src/app/workoutpage/workoutpage.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-workoutpage',
-  imports: [],
-  templateUrl: './workoutpage.html',
-  styleUrl: './workoutpage.css',
-})
-export class Workoutpage {}
